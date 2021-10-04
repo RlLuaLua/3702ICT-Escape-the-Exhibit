3dda08192d3dcd5dafaf07dd0e7c032a52247b94
RlLuaLua <87786315+RlLuaLua@users.noreply.github.com>
2021-10-04 21:34:38 +1000

    Adds Attacker functionality
    adds functionality for Chase, Patrol and Dead states

--- Assets/Scripts/AttackerFSM.cs
+++ Assets/Scripts/AttackerFSM.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackerFSM : MonoBehaviour
+public class AttackerFSM : Interactable
 {
     protected CharacterController controller;
     public enum FSMState
@@ -10,19 +10,29 @@ public class AttackerFSM : MonoBehaviour
         None,
         Patrol,
         Dead,
-        Chase,
-        Attack
+        Chase
     }
     public FSMState curState;
+    public GameObject[] waypointList;
+    public int currentWaypoint = 0;
+    public float slopeForce = 5.0f;
+    public float slopeForceRayLength = 2.0f;
 
     //ranges for State Changes
-    public float moveSpeed = 12.0f;
+    private float moveSpeed;
+    public float patrolMoveSpeed = 3.0f;
+    public float chaseMoveSpeed = 5.0f;
+    public float attackMoveSpeed = 8.0f;
+    public float attackJumpForce = 5.0f;
+    private Vector3 moveDirection;
+    
     public float chaseRange = 12.0f;
     public float attackRange = 3.0f;
     public float health = 1f;
     protected bool bDead;
 
     public float attackCoolDown;
+    protected float elapsedTime;
 
     protected Transform playerTransform;// Player Transform
     protected Vector3 destPos; // Next destination position of the NPC Tank
@@ -34,9 +44,6 @@ public class AttackerFSM : MonoBehaviour
         curState = FSMState.Patrol;
         bDead = false;
         controller = GetComponent<CharacterController>();
-        Debug.Log("playerTransform.position");
-        // Get the list of patrol points
-        //pointList = GameObject.FindGameObjectsWithTag("PatrolPoint");
 
         // Get the target (Player)
         GameObject objPlayer = GameObject.FindGameObjectWithTag("Player");
@@ -54,7 +61,6 @@ public class AttackerFSM : MonoBehaviour
             case FSMState.Patrol: UpdatePatrolState(); break;
             case FSMState.Dead: UpdateDeadState(); break;
             case FSMState.Chase: UpdateChaseState(); break;
-            case FSMState.Attack: UpdateAttackState(); break;
         }
         
         float playerDist = Vector3.Distance(transform.position, playerTransform.position);
@@ -71,17 +77,20 @@ public class AttackerFSM : MonoBehaviour
             {
                 curState = FSMState.Patrol;
             }
-            // Enter attack state when inside attack range
-            else if (playerDist <= attackRange)
-            {
-                curState = FSMState.Attack;
-            }
         }
         // Go to dead state if no health left
         else if (health <= 0)
         {
             curState = FSMState.Dead;
         }
+
+        attackCoolDown -= Time.deltaTime;
+        if (attackCoolDown < 0f){
+            attackCoolDown=0f;
+        }
+        moveDirection = new Vector3(transform.forward.x * moveSpeed, moveDirection.y, 0f);
+        moveDirection.y += (Physics.gravity.y * Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
     }
 
     // Check the collision with the player
@@ -94,25 +103,49 @@ public class AttackerFSM : MonoBehaviour
         }
     }
     //if hit by spinattack lose 1 health
-    public virtual void SpinInteract()
+    public override void SpinInteract()
     {
         Debug.Log("hit");
         health -= 1;
     }
 
     protected void UpdatePatrolState(){
+        //set move speed to patrol move speed
+        moveSpeed = patrolMoveSpeed;
+        transform.LookAt(new Vector3(waypointList[currentWaypoint].transform.position.x, transform.position.y, transform.position.z));
+            // * Raycast from controller to ground to determine if on slope. If is on slope, increase downwards force to stop bumping down slope
+            RaycastHit hit;
+            Physics.Raycast(transform.position, Vector3.down, out hit, controller.height / 2 * slopeForceRayLength);
+            if (hit.normal != Vector3.up) {
+                controller.Move(Vector3.down * controller.height / 2 * slopeForce * Time.deltaTime);
+            }
+            if (Mathf.Abs(transform.position.x - waypointList[currentWaypoint].transform.position.x) <= 0.05) {
+                if (currentWaypoint + 1 < waypointList.Length) {
+                    Debug.Log("called");
+                    currentWaypoint++;
+                }else{
+                    currentWaypoint = 0;
+                }
 
+            }
     }
 
     protected void UpdateDeadState(){
-        Debug.Log("dead");
+        moveSpeed=0f;
+        
+        Destroy(gameObject, 1.5f);
     }
 
     protected void UpdateChaseState(){
+        //set move speed to chase move speed
+        moveSpeed = chaseMoveSpeed;
+        transform.LookAt(new Vector3(playerTransform.transform.position.x, transform.position.y, transform.position.z));
 
-    }
-
-    protected void UpdateAttackState(){
-
+            // * Raycast from controller to ground to determine if on slope. If is on slope, increase downwards force to stop bumping down slope
+            RaycastHit hit;
+            Physics.Raycast(transform.position, Vector3.down, out hit, controller.height / 2 * slopeForceRayLength);
+            if (hit.normal != Vector3.up) {
+                controller.Move(Vector3.down * controller.height / 2 * slopeForce * Time.deltaTime);
+            }
     }
 }
