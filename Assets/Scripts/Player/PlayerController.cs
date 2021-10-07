b3ce79d5b41ee4f758eb33ead60afc5ca9c8f36c
Teagan <teagan.cochran@griffithuni.edu.au>
2021-10-07 13:05:23 +1000

    Clear scene and remove unnecessary scripts

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Animator animator;
+    public float moveSpeed;
+    public float turnVelocity;
+    public Vector3 velocity;
+    public bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Raycast to below the character to check if character is on ground as set isGrounded
+        if(Physics.Raycast(transform.position, Vector3.down, 0.1f)) {
+            isGrounded = true;
+        }
+        else {
+            isGrounded = false;
+        }
+        animator.SetBool("isGrounded", isGrounded);
+
+        // If grounded, reset y velocity and set animation bool for isJumping to false
+        if (isGrounded) {
+            velocity.y = -0.5f;
+            animator.SetBool("isJumping", false);
+        }
+        // Find which direction user is pressing on left or right using AD or arrow keys
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 direction = new Vector3(horizontal, 0, 0).normalized;
+        if (direction.magnitude >= 0.1f) {
+            animator.SetBool("isWalking", true);
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg, ref turnVelocity, 0.1f) ;
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            controller.Move(direction * moveSpeed * Time.deltaTime);
+        }
+        if (!Input.anyKey && isGrounded) {
+            animator.SetBool("isWalking", false);
+        }
+        if(Input.GetKey(KeyCode.W) && isGrounded) {
+            velocity.y = Mathf.Sqrt(1.0f * -2.0f * Physics.gravity.y);
+            animator.SetBool("isJumping", true);
+        }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
